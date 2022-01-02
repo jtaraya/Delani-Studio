7f4fea8c72da1a5d22d3a18c2edb1798a1204869
jtaraya <jacobtaraya@gmail.com>
2022-01-02 20:44:29 +0300

    added jquery

--- js/jquery.js
+++ js/jquery.js
@@ -1,15 +1,15 @@
 /*!
- * jQuery JavaScript Library v3.6.0
+ * jQuery JavaScript Library v3.4.1
  * https://jquery.com/
  *
  * Includes Sizzle.js
  * https://sizzlejs.com/
  *
- * Copyright OpenJS Foundation and other contributors
+ * Copyright JS Foundation and other contributors
  * Released under the MIT license
  * https://jquery.org/license
  *
- * Date: 2021-03-02T17:08Z
+ * Date: 2019-05-01T21:04Z
  */
 ( function( global, factory ) {
 
